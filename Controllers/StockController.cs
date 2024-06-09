dc7c37feae1d24e92798df78f926fbb3e5c22b8a
Austin Williams <austinw@interknowlogy.com>
2024-06-09 12:33:36 -0700

    Migrations, DBContext, DB rows init

---
+++ Controllers/StockController.cs
@@ -0,0 +1,23 @@
+﻿using FinShark.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinShark.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : Controller
+    {
+
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stock.ToList();  
+        }
+    }
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinShark.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Stock> Stocks {  get; set; }
+
+        public DbSet<Comment> Comments { get; set; } 
+    }
+}
