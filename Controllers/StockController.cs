460924f1f62c4dbc5fb058e768612e4758797fc7
Austin Williams <austinw@interknowlogy.com>
2024-06-09 14:19:23 -0700

    API setup

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -15,9 +15,24 @@ namespace FinShark.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetStocks()
         {
-            var stocks = _context.Stock.ToList();  
+            var stocks = _context.Stocks.ToList();  
+
+            return Ok(stocks);  
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStockByID([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if(stock is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);   
         }
     }
 }
